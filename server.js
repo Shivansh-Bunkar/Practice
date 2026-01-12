63c73ebda84d589d3efa4b2d8e129fb364b1fdbe
Shivansh Kumar <shivanshbunkar12345@example.com>
2026-01-12 11:17:24 +0530

    Fix port for Render deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // serve static files (HTML, CSS, JS, google file)
 app.use(express.static(path.join(__dirname, 'public')));
